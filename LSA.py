c4d4684df7e58c4e765bfb684444b073bbb9ffc3
Danil Makovey <makoveyfedorich@gmail.com>
2017-06-03 12:37:21 +0300

    Скоро защита)

--- LSA.py
+++ LSA.py
@@ -10,8 +10,8 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
 
 parser = argparse.ArgumentParser(description='File path parser')
-parser.add_argument('--source', '-s', type=str, required=True, help='Path to the file or folder with files')
-parser.add_argument('--dest', '-d', type=str, help='Path to the results saving folder')
+parser.add_argument('--source', '-s', type=str, required=True, help='Путь к файлу или папке с документами')
+parser.add_argument('--dest', '-d', type=str, help='Место сохранения итоговых данных')
 parser.add_argument('--freq', '-f', default=1, type=int, help='Если слово встречактся в документе больше раз, чем'
                                                               ' задано здесь, то оно подлежит удалению')
 parser.add_argument('--pca', action='store_true', help='Если активировать этот ключ, то будет срабатывать'
@@ -24,7 +24,7 @@ def plotGraphic(freqMatrix, docs, terms, keys):
 
     for k in range(len(freqMatrix[0])):
         # shift = random.uniform(-0.2, 0.3)
-        axes.scatter(docs[0][k], docs[1][k], docs[2][k], color='b')  # scatter - метод для нанесения маркера в точке
+        axes.scatter(docs[0][k], docs[1][k], docs[2][k], color='b')
         axes.plot([docs[0][k], docs[0][k]], [docs[1][k], docs[1][k]], zs=[docs[2][k], 0], color='k',
                   dashes=[8, 4, 2, 4, 2, 4])
         axes.text(docs[0][k], docs[1][k], docs[2][k], str(k + 1))
@@ -45,13 +45,18 @@ def plotAsPCA(fit_docs, fit_terms, keys):
     for doc in fit_docs:
         i += 1
         axes.scatter(doc[0],doc[1],doc[2], color='b')
+        axes.plot([doc[0], doc[0]], [doc[1], doc[1]], zs=[doc[2], 0], color='k',
+                  dashes=[8, 4, 2, 4, 2, 4])
         axes.text(doc[0],doc[1],doc[2], str(i))
 
     j = 0
     for term in fit_terms:
         axes.scatter(term[0],term[1],term[2], color='r')
+        axes.plot([term[0], term[0]], [term[1], term[1]], zs=[term[2], 0], color='k',
+                  dashes=[8, 4, 2, 4, 2, 4])
         axes.text(term[0],term[1],term[2], str(keys[j]))
         j += 1
+
 
 
 args = parser.parse_args()
@@ -66,11 +71,11 @@ pca = args.pca
 # if str(source.find('.txt')) == -1:
 if source[-3:len(source)] != 'txt':
     for k in range(1, 7):
-        f = open(source +'/Doc' + str(k) + '.txt')
+        f = open(source +'/Doc' + str(k) + '.txt', encoding='utf-8')
         # f = open('mydoc\\T'+str(i)+'.txt')
         words.append(f.read())
 else:
-    f = open(source)
+    f = open(source, encoding='utf-8')
     # f = open('text2.txt')
     for line in f.readlines():
         words.append(line.strip())
@@ -81,33 +86,27 @@ ST = Stemmer()
 words = [w.lower() for w in words]      # Переводим все строки в нижний регистр
 words = TP.remove_symbols(words)        # Удаляем стоп-символы
 words = TP.remove_stopwords(words)      # Удаляем стоп-слова
-print(words)
+# print(words)
 
 stemmed = []
 for sentence in words:
     s = [ST.stem(i) for i in sentence]                # Производится стемминг
     stemmed.append(s)
-print(stemmed)
+# print(stemmed)
 
-keys = TP.remove_unique(stemmed, freq)                # Удаление слов, встречающихся во всех документах более freq раз
-print(keys)                                           # По умолчанию частота freq=1 равна еденице
-print(len(keys))                                      # Получаем массив ключевых слов - термов
+keys = TP.remove_unique(stemmed, freq)                # Удаление слов, встречающихся во всех документах более freq раз/
+# print(keys)                                           # По умолчанию частота freq=1 равна еденице
+# print(len(keys))                                      # Получаем массив ключевых слов - термов
 
 table, disp_table = TP.table_generator(keys, stemmed)       # Формируем частотную матрицу - table
-print(disp_table)                                           # И таблицу частоты  встречаемости - disp_table
+# print(disp_table)                                           # И таблицу частоты  встречаемости - disp_table
 
 # Сингулярное разложение
 LA = numpy.linalg
 freqMatrix = numpy.array(table)
-print(freqMatrix)
+# print(freqMatrix)
 terms, s, docs = LA.svd(freqMatrix, full_matrices=False)
 assert numpy.allclose(freqMatrix, numpy.dot(terms, numpy.dot(numpy.diag(s), docs)))
-print(terms)
-print('          ')
-print(s)
-print('          ')
-print(docs)
-print('          ')
 s[2:] = 0
 new_a = numpy.dot(terms, numpy.dot(numpy.diag(s), docs))    # Двумерное сингулярное разложение
 # print(new_a)
@@ -116,33 +115,20 @@ new_a = numpy.dot(terms, numpy.dot(numpy.diag(s), docs))    # Двумерное
 if(pca):
     pca = PCA(n_components=3)
     fit_docs = pca.fit_transform(docs)
-    print(fit_docs)
     fit_terms = pca.fit_transform(terms)
     print(fit_terms)
+    print('          ')
+    print(fit_docs)
+    print('          ')
     plotAsPCA(fit_docs, fit_terms, keys)
 else:
+    print(terms)
+    print('          ')
+    print(s)
+    print('          ')
+    print(docs)
+    print('          ')
     plotGraphic(freqMatrix, docs, terms, keys)
-
-
-# Выводим информацию об анализе в файл
-if dest is not None:
-    print(dest)
-    plt.savefig(dest+'/graphic', fmt='png')
-    with open(dest+r'/results.txt', 'w+') as f:
-        k = 0
-        j = 0
-        f.write('Матрица термы-на-документы\n')
-        for line in disp_table:
-            f.write(str(line[::-1])+'\n')
-        f.write('Координаты термов\n')
-        for line in terms:
-            f.write(str(line[:3]) + keys[k] + '\n')
-            k += 1
-        f.write('Координаты документов\n')
-        for line in docs.transpose():             # Матрица с коорд. документов транспонирована для лучшего вывода
-            f.write(str(line[:3])+'Doc#'+str(j+1)+'\n')
-            j += 1
-    f.close()
 
 # Расчитаем расстояния до всех термов от каждого документа
 
@@ -187,5 +173,37 @@ for list in www:        # пятёрке наиболее близких и вы
     for item in list:
         sss.add(item)
 print(sss)              # Это и будут термы, наиболее точно передающие тему и смысл всего набора документов, т.е. текста
+
+# Выводим информацию об анализе в файл
+if dest is not None:
+    print(dest)
+    plt.savefig(dest+'/graphic', fmt='png')
+    with open(dest+r'/results.txt', 'w+', encoding='utf-8') as f:
+        k = 0
+        j = 0
+        f.write('Матрица термы-на-документы\n')
+        for line in disp_table:
+            f.write(str(line[::-1])+'\n')
+
+        if(pca):
+            f.write('Координаты термов\n')
+            for line in fit_terms:
+                f.write(str(line[:3]) + keys[k] + '\n')
+                k += 1
+            f.write('Координаты документов\n')
+            for line in fit_docs.transpose():  # Матрица с коорд. документов транспонирована для лучшего вывода
+                f.write(str(line[:3]) + 'Doc#' + str(j + 1) + '\n')
+                j += 1
+        else:
+            f.write('Координаты термов\n')
+            for line in terms:
+                f.write(str(line[:3]) + keys[k] + '\n')
+                k += 1
+            f.write('Координаты документов\n')
+            for line in docs.transpose():             # Матрица с коорд. документов транспонирована для лучшего вывода
+                f.write(str(line[:3])+'Doc#'+str(j+1)+'\n')
+                j += 1
+        f.write(str(sss))
+    f.close()
 
 plt.show()
